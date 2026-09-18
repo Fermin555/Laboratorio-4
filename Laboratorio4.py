71c974d05ce0df81632b27fba3b7b1240bc72db8
Fermín <ferminndelgado@gmail.com>
2026-09-18 15:22:28 -0300

    La ventana principal

---
+++ Laboratorio4.py
@@ -0,0 +1,13 @@
+# Repositorio Github: 
+# Autores: Fermin Delgado, Kevin Ulloa
+
+import tkinter as tk
+import numpy as np
+
+# 1. Crear la ventana principal
+ventana = tk.Tk()
+ventana.title("Resolución de sistemas por Cramer")
+ventana.geometry("600x400") # Define el Ancho x Alto inicial
+
+# 2. Iniciar el bucle de eventos para mantener la ventana abierta
+ventana.mainloop()
